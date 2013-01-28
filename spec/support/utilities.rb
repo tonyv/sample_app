1002066ba1d087efad5cea4fb9417eea9d445f7f
Tony Vu <anthonyqvu@gmail.com>
2013-01-27 22:29:13 -0500

    Fixed tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,27 @@
+include ApplicationHelper
+
+def valid_signin(user)
+	fill_in "Email", 			with: user.email
+	fill_in "Password",		with: user.password
+	click_button "Sign in"
+end
+
+# For tests in authentication_pages.rb
+Rspec::Matchers.define :have_error_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-error', text: message)
+	end
+end
+
+# For tests in user_pages_spec.rb
+Rspec::Matchers.define :have_success_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-success', text: message)
+	end
+end
+
+# For tests in user_spec.rb
+
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
@@ -5,4 +29,13 @@ def full_title(page_title)
 	else
 		"#{base_title} | #{page_title}"
 	end
+end
+
+def sign_in(user)
+	visit signin_path
+	fill_in "Email", 		with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token
 end
